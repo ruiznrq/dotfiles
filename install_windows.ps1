d44885f3ba30dbdad50c85b1e1db4f82aeca54a2
Enrique Ruiz Rivera <enrique.ruiz@metrohm.com>
2021-08-27 11:56:48 +0200

    Improve log messages and remove contents from Alacritty folder

--- install_windows.ps1
+++ install_windows.ps1
@@ -1,18 +1,24 @@
 # Create virtual drive W: to shortcut work folder
-$WDrivePath = "$HOME\Music"
+$WDrivePath = "$HOME\Music\"
+Write-Output "`n- Create W: virtual drive in $WDrivePath"
+
 if (-Not (Test-Path "W:")) {
-	Write-Output "W: folder does not exits, it will be crated in $WDrivePath"
+	Write-Output "`tW: drive does not exits, it will be crated in $WDrivePath"
 	New-PSDrive -Name W -PSProvider FileSystem -Root $WDrivePath -Description "Work Directory" -Scope Global -Credential (Get-Credential $user)
 }
 else {
-	Write-Output "W: already exists in $WDrivePath"
+	Write-Output "`tW: drive already exists in $WDrivePath"
 }
 
+
 # Create symbolic link to deploy dotfiles
-$AlacrittyPath = "$HOME\AppData\Roaming\alacritty"
+$AlacrittyPath = "$HOME\AppData\Roaming\alacritty\"
+Write-Output "`n- Install Alacritty configuration in $AlacrittyPath"
+
 if (Test-Path -Path $AlacrittyPath) {
-	Write-Output "Alacritty folder already exists in $AlacrittyPath, it will be deleted"
+	Write-Output "`tAlacritty folder already exists in $AlacrittyPath, it will be deleted"
+	Get-ChildItem -Path $AlacrittyPath * -Recurse | Remove-Item
 	(Get-Item $AlacrittyPath).Delete()
 }
-Write-Output "Creating Alacritty folder in $AlacrittyPath"
+Write-Output "`tCreating Alacritty folder in $AlacrittyPath"
 New-Item -ItemType SymbolicLink -Path $AlacrittyPath -Target "./alacritty"
