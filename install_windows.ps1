3918f729e858ae4fa523a274ec81d136b7c82819
Quique <ruiznrq@gmail.com>
2021-09-01 17:54:29 +0200

    New font

--- install_windows.ps1
+++ install_windows.ps1
@@ -22,6 +22,16 @@ foreach ($Font in $FontList) {
 	Copy-Item $Font "C:\Windows\Fonts"
 	New-ItemProperty -Name $Font.BaseName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $Font.name
 }
+$FontFolder = ".\fonts\\CascadiaCode"
+Write-Output "`n- Install fonts from $FontFolder"
+$FontItem = Get-Item -Path $FontFolder
+$FontList = Get-ChildItem -Path "$FontItem\*" -Include ('*.fon', '*.otf', '*.ttc', '*.ttf')
+foreach ($Font in $FontList) {
+	# TODO - Only install missing fonts
+	Write-Output '`tInstalling font -' $Font.BaseName
+	Copy-Item $Font "C:\Windows\Fonts"
+	New-ItemProperty -Name $Font.BaseName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $Font.name
+}
 
 
 # Create symbolic link to deploy Alacritty dotfiles
