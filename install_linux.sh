7a429aa85c1f6d8a78221fb4f430d343e4625d47
ruiznrq <ruiznrq@gmail.com>
2021-09-05 23:53:39 +0200

    Install alacritty and nvim

--- install_linux.sh
+++ install_linux.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-# 
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "Current directory is: $CURRENT_DIR"
 
+# Install Alacritty config
+ALACRITTY="$HOME/.config/alacritty"
+rm -rf $ALACRITTY
+ln -s "$CURRENT_DIR/alacritty" $ALACRITTY
+
+# Install NeoVim config
+NEOVIM="$HOME/.config/nvim"
+rm -rf $NEOVIM
+ln -s "$CURRENT_DIR/nvim" $NEOVIM
