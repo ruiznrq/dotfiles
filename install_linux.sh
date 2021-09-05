25f4f7aa2b2537125d49d1cd3e218d2bd956c894
ruiznrq <ruiznrq@gmail.com>
2021-09-06 00:16:44 +0200

    Install fonts for linux. Config alacritty for linux

--- install_linux.sh
+++ install_linux.sh
@@ -3,10 +3,18 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Current directory is: $CURRENT_DIR"
 
+
+# Install fonts
+HACKNF="/usr/share/fonts/truetype/Hack Nerd Font Linux/"
+if [ ! -d "$HACKNF" ]; then
+	echo "Installing Hack NF..."
+	sudo cp -r "$CURRENT_DIR/fonts/Hack Nerd Font Linux/" "/usr/share/fonts/truetype/"
+fi
+
 # Install Alacritty config
 ALACRITTY="$HOME/.config/alacritty"
 rm -rf $ALACRITTY
-ln -s "$CURRENT_DIR/alacritty" $ALACRITTY
+ln -s "$CURRENT_DIR/alacritty linux" $ALACRITTY
 
 # Install NeoVim config
 NEOVIM="$HOME/.config/nvim"
