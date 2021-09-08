05f0f8180e8bdb683c5e3c704b21b740400d43e6
Quique <ruiznrq@gmail.com>
2021-09-08 19:20:13 +0200

    LSP mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -162,6 +162,16 @@ lsp_installer.on_server_ready(function(server)
     vim.cmd [[ do User LspAttachBuffers ]]
 end)
 
+map('n', '<space>,', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+map('n', '<space>;', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+map('n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('n', '<space>d', '<cmd>lua vim.lsp.buf.definition()<CR>')
+map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+map('n', '<space>h', '<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n', '<space>m', '<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n', '<space>r', '<cmd>lua vim.lsp.buf.references()<CR>')
+map('n', '<space>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+
 -------------------- TREE-SITTER ---------------------------
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {'c', 'cpp', 'c_sharp', 'lua', 'rust', 'zig'}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
