a4fe2bd7f17b31590bbcc950287093f910c1fee8
ruiznrq <ruiznrq@gmail.com>
2021-10-12 12:22:45 +0200

    Cmp tab support

--- nvim/init.lua
+++ nvim/init.lua
@@ -76,6 +76,8 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
   },
   sources = {
     { name = 'nvim_lsp' },
